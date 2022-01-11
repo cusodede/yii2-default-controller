2d5d6efeab2ce4067ae1ed35f7da3bb279ad61db
Anton Shevelev <toxa9393@yandex.ru>
2022-01-11 16:05:44 +0300

    create extension Default Controller

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# yii2-default-controller
-Обобщённый контроллер + view по умолчанию
+Default Controller
+==================
+Default Controller
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist cusodede/yii2-default-controller "*"
+```
+
+or add
+
+```
+"cusodede/yii2-default-controller": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \cusodede\DefaultController\AutoloadExample::widget(); ?>```
---
+++ src/DefaultController.php
@@ -0,0 +1,78 @@
+<?php
+declare(strict_types = 1);
+
+namespace cusodede\DefaultController;
+
+use app\components\helpers\ArrayHelper;
+use pozitronik\helpers\ControllerHelper;
+use pozitronik\traits\traits\ControllerTrait;
+use Yii;
+use yii\web\Controller;
+
+/**
+ * This is just an example.
+ */
+class DefaultController extends Controller {
+	use ControllerTrait;
+
+	/**
+	 * @var string название поля с первичным ключом.
+	 * Конечно ключ может быть составным, но пока таких случаев не встречалось
+	 */
+	protected string $primaryColumnName = 'id';
+
+	/**
+	 * Название контроллера
+	 */
+	protected const DEFAULT_TITLE = null;
+
+	/**
+	 * Дефолтные названия экшенов
+	 */
+	protected const ACTION_TITLES = [
+		'view' => 'Просмотр',
+		'edit' => 'Редактирование',
+		'create' => 'Создание',
+		'import' => 'Загрузка',
+		'import-status' => 'Статус загрузки'
+	];
+
+	/**
+	 * @var string|null $modelClass
+	 */
+	public ?string $modelClass = null;
+	/**
+	 * @var string|null $modelSearchClass
+	 */
+	public ?string $modelSearchClass = null;
+
+	/**
+	 * @var bool enablePrototypeMenu
+	 */
+	public bool $enablePrototypeMenu = true;
+
+	/**
+	 * @return string
+	 */
+	public static function Title():string {
+		return static::DEFAULT_TITLE??ControllerHelper::ExtractControllerId(static::class);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function beforeAction($action):bool {
+		$this->view->title = $this->view->title??ArrayHelper::getValue(static::ACTION_TITLES, $action->id, self::Title());
+		if (!isset($this->view->params['breadcrumbs'])) {
+			if ($this->defaultAction === $action->id) {
+				$this->view->params['breadcrumbs'][] = self::Title();
+			} else {
+				$actionForUrl = Yii::$app->id !== $this->module->id?'/'.$this->module->id.$this::to($this->defaultAction):$this::to($this->defaultAction);
+				$this->view->params['breadcrumbs'][] = ['label' => self::Title(), 'url' => $actionForUrl];
+				$this->view->params['breadcrumbs'][] = ['label' => $this->view->title];
+			}
+
+		}
+		return parent::beforeAction($action);
+	}
+}
